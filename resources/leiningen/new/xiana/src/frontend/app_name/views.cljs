3a4d785fdfc331c19ed619a9530911c4f7d2dca6
Ian Fernandez <d.ian.b@live.com>
2021-03-30 13:27:31 -0300

    Add example test for main-panel

--- resources/leiningen/new/xiana/src/frontend/app_name/views.cljs
+++ resources/leiningen/new/xiana/src/frontend/app_name/views.cljs
@@ -7,5 +7,6 @@
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "Hello from " @name]
+     [:h1 {:data-testid "hello"}
+      "Hello from " @name]
      ]))
