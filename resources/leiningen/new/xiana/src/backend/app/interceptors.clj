ce4fecfe4d3d598c73e2dcb46353b8e1b1440345
dimos-flexiana <dimos.michailidis@flexiana.com>
2021-04-22 18:12:21 +0300

    bring back the interceptor sample and add the :leave branch

---
+++ resources/leiningen/new/xiana/src/backend/app/interceptors.clj
@@ -0,0 +1,9 @@
+(ns interceptors
+  (:require
+    [xiana.core :as xiana]))
+
+(def sample-{{sanitized-name}}-controller-interceptor
+  {:enter (fn [{request :request {:keys [handler controller match]} :request-data :as state}]
+            (xiana/ok state))
+   :leave (fn [{response :response :as state}]
+            (xiana/ok state))})
