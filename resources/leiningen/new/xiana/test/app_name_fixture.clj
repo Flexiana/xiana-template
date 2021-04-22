907dd1bec9d9f77c729fc8a671eee48a508feabd
dimos-flexiana <dimos.michailidis@flexiana.com>
2021-04-22 17:51:35 +0300

    bring back migrate! function into test/app_name_fixture namespace

--- resources/leiningen/new/xiana/test/app_name_fixture.clj
+++ resources/leiningen/new/xiana/test/app_name_fixture.clj
@@ -1,5 +1,6 @@
 (ns {{sanitized-name}}-fixture
   (:require
+    [migratus.core :as migratus]
     [next.jdbc :as jdbc]
     [framework.config.core :as config]
     [com.stuartsierra.component :as component]
@@ -22,6 +23,13 @@
                         :subname (str "//localhost:" pg-port "/{{sanitized-name}}")))]
     (jdbc/execute! (dissoc db-config :dbname) [init-sql])
     (assoc config :framework.db.storage/postgresql db-config)))
+
+(defn migrate!
+  [config]
+  (let [db (:framework.db.storage/postgresql config)
+        mig-config (assoc (:framework.db.storage/migration config) :db db)]
+    (migratus/migrate mig-config))
+  config)
 
 (defn std-system-fixture
   [f]
