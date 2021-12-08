25b305838be8cab2202b4a25230ba37d9b7a4ef3
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-12-08 10:33:33 +0100

    move it to com.flexiana package

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject xiana/lein-template "0.1.0"
-  :description "A Leiningen template for flexiana/framework"
+(defproject com.flexiana/lein-template "0.3.0"
+  :description "A Leiningen template for flexiana/framework v0.3.x"
   :url "https://github.com/Flexiana/xiana-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
