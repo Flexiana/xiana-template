670ec23ad376690129c1237d1ed03839a61ddf0c
Seçkin KÜKRER <seckin.kukrer@hotmail.com>
2022-10-19 17:09:56 +0300

    core re-factored to handle Swagger Integration.

--- resources/leiningen/new/xiana/src/backend/app_name/core.clj
+++ resources/leiningen/new/xiana/src/backend/app_name/core.clj
@@ -7,21 +7,40 @@
     [xiana.interceptor :as interceptors]
     [xiana.rbac :as rbac]
     [xiana.route :as routes]
+    [xiana.swagger :as xsw]
     [xiana.session :as session]
     [xiana.webserver :as ws]
     [reitit.ring :as ring]
+    [clojure.walk]
+    [ring.util.response]
     [xiana.commons :refer [rename-key]]))
 
 (def routes
   [["/" {:action #'index/handle-index}]
    ["/re-frame" {:action #'re-frame/handle-index}]
-   ["/assets/*" (ring/create-resource-handler {:path "/"})]])
+   ["/assets/*" (ring/create-resource-handler {:path "/"})]
+
+   ["/swagger-ui" {:no-doc true
+                   :get {:action
+                         (fn [state]
+                           (assoc state
+                                  :response
+                                  (-> "swaggerui.html"
+                                      (ring.util.response/resource-response {:root "public"})
+                                      (ring.util.response/header "Content-Type" "text/html; charset=utf-8"))))}}]
+   ^{:no-doc true}
+   ["/swagger.json" {:action (fn [state]
+                               (assoc state
+                                      :response
+                                      (ring.util.response/response
+                                       (str (clojure.walk/stringify-keys (-> state :deps :swagger-data))))))}]])
 
 (defn ->system
   [app-cfg]
   (-> (config/config app-cfg)
       (rename-key :framework.app/auth :auth)
       routes/reset
+      xsw/->swagger-data
       rbac/init
       session/init-backend
       db/connect
