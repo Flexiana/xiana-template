17e606f4d042fa1fe9d293f6c4183f29752ba0e9
gmsvalente <gustavomsvalente@gmail.com>
2023-11-02 08:52:30 -0300

    Update version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject xiana/lein-template "0.5.0.10"
-  :description "A Leiningen template for flexiana/framework v0.5.0-rc3"
+(defproject xiana/lein-template "0.5.0-rc4"
+  :description "A Leiningen template for flexiana/framework v0.5.0-rc4"
   :url "https://github.com/Flexiana/xiana-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
